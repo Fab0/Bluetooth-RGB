6570b83249bc66af7779df74c9d850af498539ac
Fabian <Frank90@gmx-topmail.de>
2014-01-03 16:36:50 +0100

    v0.2

--- BluetoothRGB/src/com/fab/bluetoothrgb/Fragment1.java
+++ BluetoothRGB/src/com/fab/bluetoothrgb/Fragment1.java
@@ -5,15 +5,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
  
 public class Fragment1 extends Fragment {
  
+	private Button button_fade_color;
+	private Button button_const_color;
+	private CustomColorPicker picker;
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
- 
+    	
+    	View rootView0 = inflater.inflate(R.layout.fragment0, container, false);
         View rootView = inflater.inflate(R.layout.fragment1, container, false);
-         
+        
+        button_fade_color = (Button) rootView.findViewById(R.id.button_fade);
+        button_const_color = (Button) rootView.findViewById(R.id.button_hsv);
+        
+        picker = (CustomColorPicker) rootView0.findViewById(R.id.picker);
+        
+        button_fade_color.setOnClickListener(new OnClickListener() {
+    		
+    		@Override
+    		public void onClick(View v) {;
+    			((MainActivity) getActivity()).sendMessage("11");
+    		}
+    	});
+    	
+        button_const_color.setOnClickListener(new OnClickListener() {
+    		
+    		@Override
+    		public void onClick(View v) {;
+    			((MainActivity) getActivity()).sendMessage("10");
+    		}
+    	});
         return rootView;
     }
 }
